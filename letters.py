d9b7141ab0c3cdb41892091a9785c8b35236be3c
AuraBQ <auradii@hotmail.com>
2024-10-29 09:16:00 -0700

    Update letters.py

--- letters.py
+++ letters.py
@@ -1,9 +1,10 @@
 def main():
     names = ["Mario", "Luigi", "Daisy", "Yoshi"]
+    # name pulls every individual name in the list, then prints each name in a separate letter in the {receiver} section.
     for name in names:
         print(write_letter(name, "Princess Peach"))
 
-
+# Uses receiver and sender to insert the corresponding names into the letter with an f string.
 def write_letter(receiver, sender):
     return f"""
         +~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+
