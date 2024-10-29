0870fe27d9d44973119d270b97e9a260e5e7136f
AuraBQ <auradii@hotmail.com>
2024-10-28 20:11:46 -0700

    Create hogwarts.py

---
+++ hogwarts.py
@@ -0,0 +1,50 @@
+# Demonstrates iterating over a list of dict objects
+
+#students = [
+#    {"name": "Hermione", "house": "Gryffindor", "patronus": "Otter"},
+#    {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"},
+#    {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russell terrier"},
+#    {"name": "Draco", "house": "Slytherin", "patronus": None},
+#]
+
+#for student in students:
+#    print(student["name"], student["house"], student["patronus"], sep=", ")
+
+# Demonstrates iterating over and index into a dict
+
+#students = {
+#    "Hermione": "Gryffindor",
+#    "Harry": "Gryffindor",
+#    "Ron": "Gryffindor",
+#    "Draco": "Slytherin",
+#}
+
+#for student in students:
+#    print(student, students[student], sep=", ")
+
+# Demonstrates indexing into a dict
+
+#students = {
+#    "Hermione": "Gryffindor",
+#    "Harry": "Gryffindor",
+#    "Ron": "Gryffindor",
+#    "Draco": "Slytherin",
+#}
+
+#print(students["Hermione"])
+#print(students["Harry"])
+#print(students["Ron"])
+#print(students["Draco"])
+
+# Demonstrates iterating over a list
+
+#students = ["Hermione", "Harry", "Ron"]
+
+#for student in students:
+#    print(student)
+
+#students = ["Hermione", "Harry", "Ron"]
+
+#print(students[0])
+#print(students[1])
+#print(students[2])
